5fcb74384dc5a8cab47efd5117e68e30f8e5a5d3
Diego Santana <3c.santana@gmail.com>
2024-08-20 19:40:34 -0300

    test: Add method test nome == null

--- src/test/java/com/example/finans/domain/UsuarioTest.java
+++ src/test/java/com/example/finans/domain/UsuarioTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.example.finans.domain.exception.ValidationException;
+
 class UsuarioTest {
 
 	@Test
@@ -18,5 +20,15 @@ class UsuarioTest {
 				() -> assertEquals("1234", user.getSenha())
 		);
 	}
+	
+	@Test
+	@DisplayName("Deve lançar uma exceção: usuário sem nome")
+	void deveRejeitarUmUsuarioSemNome() {
+		ValidationException ex = assertThrows(ValidationException.class, () -> 
+			new Usuario(1L, null, "bob@email.com", "1234"));
+		assertEquals("Nome é obrigatório", ex.getMessage());
+	}
+	
+
 
 }
