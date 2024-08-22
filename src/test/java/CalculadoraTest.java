d407e7608f74cc1647328495e1df5d4a8208cef6
Diego Santana <3c.santana@gmail.com>
2024-08-22 05:24:17 -0300

    feat: Aula sobre ParameterizedTest

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,5 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculadoraTest {
 
@@ -25,4 +29,29 @@ public class CalculadoraTest {
 			float result = 10 / 07;
 		});
 	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"Maça", "Pera", "Uva"})
+	public void testStrings(String str) {
+		assertNotNull(str);
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"5, 2, 7",
+		"4, 8, 12",
+		"1, 3, 4"
+	})
+	public void testSomarComParams(int value1, int value2, int result) {
+		Calculadora calc = new Calculadora();
+		assertTrue(calc.soma(value1, value2) == result);
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/sumParams.csv")
+	public void testSomarParamsComCsvFile(int value1, int value2, int result) {
+		Calculadora calc = new Calculadora();
+		assertTrue(calc.soma(value1, value2) == result);
+	}
+
 }
