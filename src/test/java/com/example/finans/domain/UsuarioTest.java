cd0b3db0bf394b76030c6929c777d34b82799bfd
Diego Santana <3c.santana@gmail.com>
2024-08-24 20:25:16 -0300

    refactor: Organiza os imports

--- src/test/java/com/example/finans/domain/UsuarioTest.java
+++ src/test/java/com/example/finans/domain/UsuarioTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
+//import org.junit.jupiter.params.provider.CsvSource;
 
 import com.example.finans.domain.exception.ValidationException;
 
