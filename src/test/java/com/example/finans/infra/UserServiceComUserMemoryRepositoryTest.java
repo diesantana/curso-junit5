e1a9b4f0b17f9519be330c4b818cd092a5312210
Diego Santana <3c.santana@gmail.com>
2024-09-06 19:21:51 -0300

    feat: aula sobre ordem de execução de testes e porque não deve ser feito

---
+++ src/test/java/com/example/finans/infra/UserServiceComUserMemoryRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.finans.infra;
+
+import static com.example.finans.domain.builders.UsuarioBuilderByMaster.umUsuario;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import com.example.finans.domain.Usuario;
+import com.example.finans.domain.exception.ValidationException;
+import com.example.finans.service.UsuarioService;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UserServiceComUserMemoryRepositoryTest {
+
+	private static UsuarioService service = new UsuarioService(new UsuarioMemoryRepository());
+	
+	
+	@Test
+	@Order(1)
+	void deveSalvarUsuarioValido() {
+		Usuario user = service.salvar(umUsuario().comId(null).agora());
+		assertNotNull(user.getId());
+	}
+	
+	@Test
+	@Order(2)
+	void deveRejeitarUsuarioExistente() {
+		ValidationException exception = assertThrows(ValidationException.class, () -> {
+			service.salvar(umUsuario().comId(null).agora());
+		});
+		assertEquals("Usuário bob@email.com já cadastrado!", exception.getMessage());
+	}
+}
