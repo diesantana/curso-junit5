9cd7bc8a47b104d5afc6abef974f670df8acb69d
Diego Santana <3c.santana@gmail.com>
2024-10-03 05:43:19 -0300

    test: Testando a validação do método salvar

--- src/main/java/com/example/finans/service/TransacaoService.java
+++ src/main/java/com/example/finans/service/TransacaoService.java
@@ -13,10 +13,10 @@ public class TransacaoService {
 	}
 	
 	public Transacao salvar(Transacao transacao) {
-		if(transacao.getDescricao() == null) throw new ValidationException("Transação insexistente");
-		if(transacao.getValor() == null) throw new ValidationException("Valor insexistente");
-		if(transacao.getConta() == null) throw new ValidationException("Conta insexistente");
-		if(transacao.getData() == null) throw new ValidationException("Data insexistente");
+		if(transacao.getDescricao() == null) throw new ValidationException("Descrição inexistente");
+		if(transacao.getValor() == null) throw new ValidationException("Valor inexistente");
+		if(transacao.getConta() == null) throw new ValidationException("Conta inexistente");
+		if(transacao.getData() == null) throw new ValidationException("Data inexistente");
 		if(transacao.getStatus() == null) transacao.setStatus(false);
 		
 		return dao.salvar(transacao);
--- src/test/java/com/example/finans/service/TransacaoServiceTest.java
+++ src/test/java/com/example/finans/service/TransacaoServiceTest.java
@@ -1,18 +1,29 @@
 package com.example.finans.service;
 
+import static com.example.finans.domain.builders.ContaBuilder.umConta;
 import static com.example.finans.domain.builders.TransacaoBuilder.umaTransacao;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.example.finans.domain.Conta;
 import com.example.finans.domain.Transacao;
+import com.example.finans.domain.exception.ValidationException;
 import com.example.finans.service.repositories.TransacaoDao;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,4 +53,29 @@ public class TransacaoServiceTest {
 				)
 		);
 	}
+	
+	@ParameterizedTest(name = "{5}")
+	//@MethodSource("invalidDataParameterized")
+	@MethodSource()
+	@DisplayName("Método salvar dele lançar uma ValidationException")
+	void salvarDeveLancarExcecaoQuandoDadosForemInvalidos(String descricao, 
+			Double valor, Conta conta, LocalDate data, Boolean status, String expectedMsg) {
+		// Arrange - Given
+		Transacao transacao = umaTransacao().comDescricao(descricao)
+				.comValor(valor).comConta(conta).comData(data).comStatus(status).agora();
+		ValidationException exception = assertThrows(ValidationException.class, () -> {
+			service.salvar(transacao);
+		});
+		assertEquals(expectedMsg, exception.getMessage());
+	}
+	
+	static Stream<Arguments> salvarDeveLancarExcecaoQuandoDadosForemInvalidos() {
+		return Stream.of(
+			Arguments.of(null, 100.0, umConta().agora(), LocalDate.now(), true, "Descrição inexistente"),
+			Arguments.of("Uma Descrição", null, umConta().agora(), LocalDate.now(), true, "Valor inexistente"),
+			Arguments.of("Uma Descrição", 100.0, null, LocalDate.now(), true, "Conta inexistente"),
+			Arguments.of("Uma Descrição", 100.0, umConta().agora(), null, true, "Data inexistente")
+		);
+	}
+	
 }
